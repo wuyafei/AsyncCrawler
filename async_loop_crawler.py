959fa3a4277a30b47ade26ea49b9b8189344086e
wuyafei <wuyafei@outlook.com>
2016-05-18 20:00:42 +0800

    not tested

---
+++ async_loop_crawler.py
@@ -0,0 +1,91 @@
+# -*- conding: utf8-*-
+from selectors import *
+import re
+import socket
+import urllib.parse
+
+selector = DefaultSelector()
+urls_todo = set(['/'])
+urls_seen = set(['/'])
+stopped = False
+
+
+class Fetcher:
+
+    def __init__(self, url):
+        self.url = url
+        self.response = b''
+        self.sock = None
+
+    def fetch(self):
+        self.sock = socket.socket()
+        self.sock.setblocking(False)
+        try:
+            self.sock.connect(('xkcd.com', 80))
+        except BlockingIOError:
+            pass
+        selector.register(self.sock.fileno(), EVENT_WRITE, self.connected)
+
+    def connected(self, key, mask):
+        print 'connected'
+        selector.unregister(key.fd)
+        request = 'get {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n'.format(self.url)
+        self.sock.send(request.encode('ascii'))
+        selector.register(key.fd, EVENT_READ, self.read_response)
+
+    def read_response(self, key, mask):
+        global stopped
+        chunk = self.sock.recv(4096)
+        if chunk:
+            self.response += chunk
+        else:
+            selector.unregister(key.fd)
+            links = self.parse_links()
+            for link in links.difference(urls_seen):
+                urls_todo.add(link)
+                Fetcher(link).fetch()
+            urls_seen.update(links)
+            urls_todo.remove(self.url)
+            if not urls_todo:
+                stopped = True
+
+    def _is_html(self):
+        headers, body = self.response.split(b'\r\n\r\n')
+        headers = dict(head.split(': ') for head in headers.decode().split('\r\n')[1:])
+        return headers.get('Content-Type', '').startswith('text/html')
+
+    def body(self):
+        body = self.response.split(b'\r\n\r\n', 1)[1]
+        return body.decode('utf-8')
+
+    def parse_links(self):
+        if not self.response:
+            print 'error: {}'.format(self.url)
+            return set()
+        if not self._is_html():
+            return set()
+        urls = set(re.findall(r'''(?i)href=['"]?([^\s<>'"]+)''', self.body()))
+        links = set()
+        for url in urls:
+            norm_url = urllib.parse.urljoin(self.url, url)
+            url_parts = urllib.parse.urlparse(norm_url)
+            if url_parts.scheme not in ('', 'http', 'https'):
+                continue
+            host, port = urllib.parse.splitport(url_parts.netloc)
+            if host and host.lower() not in ('xkcd.com', 'www.xkcd.com'):
+                continue
+            defragmented, frag = urllib.parse.urldefrag(url_parts.path)
+            links.add(defragmented)
+        return links
+
+
+def loop():
+    while not stopped:
+        events = selector.select()
+        for event_key, event_mask in events:
+            callback = event_key.data
+            callback(event_key, event_mask)
+
+if __name__ == '__main__':
+    Fetcher('/').fetch()
+    loop()
---
+++ block_fetch.py
@@ -0,0 +1,24 @@
+# -*-conding: utf8-*-
+import socket
+import re
+
+
+def fetch(url):
+    sock = socket.socket()
+    sock.connect(('xkcd.com', 80))
+    request = 'GET {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n'.format(url)
+    sock.send(request.encode('ascii'))
+    response = b''
+    chunk = sock.recv(4096)
+    while chunk:
+        response += chunk
+        chunk = sock.recv(4096)
+
+    # Page is now downloaded.
+    urls = set(re.findall(r'''(?i)href=["']?([^\s"'<>]+)''',
+                   response.split(b'\r\n\r\n', 1)[1].decode('utf-8')))
+    print urls
+
+
+if __name__ == '__main__':
+    fetch('/')
